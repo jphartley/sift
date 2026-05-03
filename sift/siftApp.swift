f08366456886e6657792754476ead6559f298371
Jeremy Hartley <jphartley@gmail.com>
2026-05-03 10:24:17 +0200

    Initial Commit

---
+++ sift/siftApp.swift
@@ -0,0 +1,17 @@
+//
+//  siftApp.swift
+//  sift
+//
+//  Created by Jeremy Hartley on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct siftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
